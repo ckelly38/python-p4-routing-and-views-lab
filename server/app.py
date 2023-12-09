8c5f959da1e203c5682ce39b73c5b0d476191d04
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-12-09 02:33:49 -0700

    this demonstrates flask routes and simple math

--- server/app.py
+++ server/app.py
@@ -4,5 +4,40 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>";
+
+@app.route("/print/<parameter>")
+def print_string(parameter):
+    print(parameter);
+    return parameter;
+
+@app.route("/count/<int:parameter>")
+def count(parameter):
+    #if (type(parameter) == int and (0 == parameter or 0 < parameter)): pass;
+    #else: raise Exception("parameter must be a number!");
+    mystr = "";
+    for c in range(parameter):
+        mystr += str(c) + "\n";
+        print(c);
+    return mystr;
+
+@app.route("/math/<int:parama>/<operation>/<int:paramb>")
+def math(parama, operation, paramb):
+    #print(f"parama = {parama}");
+    #print(f"operation = {operation}");
+    #print(f"paramb = {paramb}");
+    res = 0;
+    if (operation == "*"): res = parama * paramb;
+    elif (operation == "div"): res = parama / paramb;
+    elif (operation == "+"): res = parama + paramb;
+    elif (operation == "-"): res = parama - paramb;
+    elif (operation == "%"): res = parama % paramb;
+    elif (operation == "^"): res = math.pow(parama, paramb);
+    else: raise Exception("invalid operation!");
+    #print(f"res = {res}");
+    return str(res);
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
